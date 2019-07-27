e830ca8f46f1bb938302c8a68285ac2dfb1afa36
katsuya.shiraki <asth212riw@gmail.com>
2019-07-27 11:26:10 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 白木克弥 on 2019/07/27.
+//  Copyright © 2019 katsuya.shiraki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
